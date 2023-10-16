16f4c83b646f0943ab6d55495c00f56e824e9488
Harsh <harshmehra53@gmail.com>
2023-10-16 16:58:11 +0530

    up

--- Basics_1/index.js
+++ Basics_1/index.js
@@ -1,8 +1,21 @@
 //--> alert("Hello World!");  //-- Alert window function
-let myFirstName = "Harsh";
+let myFirstName = 'Harsh';
 console.log(typeof myFirstName); //-- Here typeof operator is used to check data type of myFirstName;
 
 let car;
 console.log(typeof car); //-- Undefined data type
 
 console.log(typeof null); //-- This is an bug
+
+//--> Concatenate two strings
+let fName = 'Harsh';
+let lName = 'Mehra';
+console.log('Harsh' + ' ' + 'Mehra');
+
+//--> Template Litreals
+const firstName = 'Harsh';
+const lastName = 'Mehra';
+const birthYear = 1992;
+
+const aboutMe = `I'am ${firstName} ${lastName} and I was born in ${birthYear}`;
+console.log(aboutMe);
