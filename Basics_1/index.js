be7a1abbdbe32eeed13956d759abf34faa72a64f
Harsh <harshmehra53@gmail.com>
2023-10-16 11:48:54 +0530

    up

--- Basics_1/index.js
+++ Basics_1/index.js
@@ -1,1 +1,8 @@
 //--> alert("Hello World!");  //-- Alert window function
+let myFirstName = "Harsh";
+console.log(typeof myFirstName); //-- Here typeof operator is used to check data type of myFirstName;
+
+let car;
+console.log(typeof car); //-- Undefined data type
+
+console.log(typeof null); //-- This is an bug
