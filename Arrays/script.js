39fdb4931c568ce3d6d870f53235d2e6f79db65e
Harsh <harshmehra53@gmail.com>
2023-10-21 18:59:55 +0530

    upd

--- Arrays/script.js
+++ Arrays/script.js
@@ -32,3 +32,15 @@ const ages = [
   calcAge(years[3]),
 ];
 console.log(ages);
+
+//--> ARRAY METHODS
+const cars = new Array('Elevate', 'Vitus', 'Slavia');
+console.log(cars);
+
+cars.push('Fortuner', 'Jazz'); //--> .push() used to add element in array in last.
+cars.unshift('Honda', 'Volkswagen'); //--> .unshift() used to add elements in starting of an array
+
+// const newCars = cars.unshift('Honda', 'Volkswagen');    //--> We can use this method storing method into variable to find the new size of an array.
+cars.pop(); //--> .pop() used to remove last element from an array
+cars.shift(); //--> .shift() used to remove first element of an array
+console.log(cars.indexOf('Elevate')); //--> .indexof() gives index value of an element
